71d9430aee341b6f8a99a5a9b9c498e5f07c4718
senthh <senthil.kumar@acceldata.io>
2025-04-02 12:33:48 +0530

    Added Time functions test

--- src/main/scala/TimeTest.scala
+++ src/main/scala/TimeTest.scala
@@ -7,9 +7,13 @@ object TimeTest {
       .appName("SparkSubmitApp")
       .master("local[*]")
       .getOrCreate()
-    spark.sql("select tiME '10:57:00' as col").show()
-    spark.sql("select HOUR(TIME'10:57:00') as col").show()
-    // spark.sql("select HOUR(TIME'10:57:00') as col").show()
+    spark.sql("select tiME '10:57:00' as TIME").show()
+    spark.sql("select HOUR(TIME'10:57:00') as HOUR").show()
+    spark.sql("select time('12:25:13.45') as TIME").show()
+    spark.sql("select time(timestamp'2020-04-30 00:25:13.45') as TIME").show()
+    spark.sql("select time(123) as TIME").show()
+    spark.sql("select time(231) as TIME").show()
+    spark.sql("select time(timestamp'2020-04-30 12:25:13.45') as TIME").show()
   }
 }
 
